7f1da426f76ce8215e54e8a66931716687d41e56
Ali Aboutalebi <aboutalebi@live.com>
2017-11-26 10:01:06 +0330

    compress/decompress viewstate

--- AliaaCommon/Compressor.cs
+++ AliaaCommon/Compressor.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Web;
+using System.Web.UI;
 
 namespace AliaaCommon
 {
@@ -18,7 +19,7 @@ namespace AliaaCommon
             gzip.Close();
             return output.ToArray();
         }
-
+        
         public static byte[] Decompress(byte[] data)
         {
             MemoryStream input = new MemoryStream();
@@ -37,6 +38,36 @@ namespace AliaaCommon
             }
             gzip.Close();
             return output.ToArray();
+        }
+
+        /// <summary>
+        /// Call this method on page's "LoadPageStateFromPersistenceMedium" method
+        /// </summary>
+        /// <param name="request">Request object of page</param>
+        /// <returns>return this as "LoadPageStateFromPersistenceMedium" method result </returns>
+        public static object DecompressViewState(HttpRequest request)
+        {
+            string viewState = request.Form["__VSTATE"];
+            byte[] bytes = Convert.FromBase64String(viewState);
+            bytes = Decompress(bytes);
+            LosFormatter formatter = new LosFormatter();
+            return formatter.Deserialize(Convert.ToBase64String(bytes));
+        }
+
+        /// <summary>
+        /// Call this method on page's "SavePageStateToPersistenceMedium" method
+        /// </summary>
+        /// <param name="clientScript">ClientScript object of page</param>
+        /// <param name="viewState">parameter of "SavePageStateToPersistenceMedium" method</param>
+        public static void CompressViewState(ClientScriptManager clientScript, object viewState)
+        {
+            LosFormatter formatter = new LosFormatter();
+            StringWriter writer = new StringWriter();
+            formatter.Serialize(writer, viewState);
+            string viewStateString = writer.ToString();
+            byte[] bytes = Convert.FromBase64String(viewStateString);
+            bytes = Compress(bytes);
+            clientScript.RegisterHiddenField("__VSTATE", Convert.ToBase64String(bytes));
         }
     }
 }
