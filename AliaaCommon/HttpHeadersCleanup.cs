1853fd22eca2e63c771e36c9db09ec2d03699a18
Ali Aboutalebi <aboutalebi@live.com>
2018-01-17 11:18:04 +0330

    clear sensitive http response headers

---
+++ AliaaCommon/HttpHeadersCleanup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AliaaCommon
+{
+    public class HttpHeadersCleanup : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += PreSendRequestHeaders;
+        }
+
+        private static void PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Remove("Server");
+            HttpContext.Current.Response.Headers.Remove("X-AspNet-Version");
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
